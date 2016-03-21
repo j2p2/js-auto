4eb3e5157e42f935a806d141a2c8becf84aff0c5
j2p2 <j2pierce@gmail.com>
2016-03-20 20:59:30 -0500

    added selenium stuff to the tests, but they aren't working at the moment for some reason (neat!)

--- test/features/search.feature
+++ test/features/search.feature
@@ -1,5 +1,5 @@
 Feature: Search functionality
 
 Scenario: Search for a query
-Given I submit a search query "test"
+Given I submit a search query 'test'
 Then I should reach the search results page
--- test/stepdefs/search-stepdef.js
+++ test/stepdefs/search-stepdef.js
@@ -1,19 +1,42 @@
 var assert = require('assert');
 var English = require('yadda').localisation.English;
 var Wall = require('../../lib/search'); // The library that you wish to test
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
 
 module.exports = (function() {
   return English.library()
+  
     .given("$NUM green bottles are standing on the wall", function(number, next) {
        wall = new Wall(number);
        next();
     })
+	
     .when("$NUM green bottle accidentally falls", function(number, next) {
-       wall.fall(number);
-       next();
+		wall.fall(number);
+		next();
     })
+	
     .then("there are $NUM green bottles standing on the wall", function(number, next) {
        assert.equal(number, wall.bottles);
        next();
-    });
+    })
+	
+	.given("I submit a search query '$query'", function(query, next) {
+		driver.get('http://www.google.com/');
+		driver.findElement(By.name('q')).sendKeys('webdriver');
+		driver.findElement(By.name('btnG')).click();
+		driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+		driver.quit();
+		next();
+	})
+	
+	.then("I should reach the search results page", function(next) {
+		next();
+	});
 })();
