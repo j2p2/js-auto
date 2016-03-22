c52bb36d828402c6df054af5efc55b2ba2dcb05c
j2p2 <j2pierce@gmail.com>
2016-03-22 13:30:43 -0500

    fixed selenium portion - this is almost certainly terrible code

--- search-test.js
+++ search-test.js
@@ -1,17 +1,57 @@
+/* jslint node: true */
+/* global before, afterEach, after, featureFile, scenarios, steps */
+"use strict";
+
 var Yadda = require('yadda');
 Yadda.plugins.mocha.StepLevelPlugin.init();
 
+var library = require('./test/stepdefs/search-stepdef');
+var webdriver = require('selenium-webdriver');
+
+var fs = require('fs');
+var driver;
+
 new Yadda.FeatureFileSearch('./test/features').each(function(file) {
+    featureFile(file, function(feature) {
 
-  featureFile(file, function(feature) {
+        before(function(done) {
+            executeInFlow(function() {
+                driver = new webdriver.Builder().forBrowser('firefox').build();
+                driver.manage().timeouts().implicitlyWait(10000);
+				//By = webdriver.By;
+				//until = webdriver.until;
+            }, done);
+        });
 
-    var library = require('./test/stepdefs/search-stepdef');
-    var yadda = Yadda.createInstance(library);
+        scenarios(feature.scenarios, function(scenario) {
+            steps(scenario.steps, function(step, done) {
+                executeInFlow(function() {
+                    Yadda.createInstance(library, { driver: driver }).run(step);
+                }, done);
+            });
+        });
 
-    scenarios(feature.scenarios, function(scenario) {
-      steps(scenario.steps, function(step, done) {
-        yadda.run(step, done);
-      });
+        afterEach(function() {
+            takeScreenshotOnFailure(this.currentTest);
+        });
+
+        after(function(done) {
+            driver.quit().then(done);
+        });
     });
-  });
 });
+
+function executeInFlow(fn, done) {
+    webdriver.promise.controlFlow().execute(fn).then(function() {
+        done();
+    }, done);
+}
+
+function takeScreenshotOnFailure(test) {
+    if (test.state != 'passed') {
+        var path = 'screenshots/' + test.title.replace(/\W+/g, '_').toLowerCase() + '.png';
+        driver.takeScreenshot().then(function(data) {
+            fs.writeFileSync(path, data, 'base64');
+        });
+    }
+}
